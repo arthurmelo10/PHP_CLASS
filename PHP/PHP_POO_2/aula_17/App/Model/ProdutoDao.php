ce26692aadd347d13aac7e3d019cd9c909135a4a
arthur_melo <arthurmelo1000@gmail.com>
2022-04-25 20:53:48 -0300

    CRUD com banco de Dados

--- PHP/PHP_POO_2/aula_17/App/Model/ProdutoDao.php
+++ PHP/PHP_POO_2/aula_17/App/Model/ProdutoDao.php
@@ -25,24 +25,42 @@
         {
             $sql = 'SELECT * FROM produtos';
 
+
             $stmt = Conexão::getConn()->prepare($sql);
             $stmt->execute();
             
             //necessário verificar se está consulta retornou algum resultado
 
             if ($stmt->rowCount() > 0) { //rowCount() -> OBTER A QUANTIDADE DE REGISTROS NO BANCO DE DADOS
-                $resultado = $stmt->fetchAll(\PDO::FETCH_ASSOC);
+                $resultado = $stmt->fetchAll(\PDO::FETCH_ASSOC); //executamos a query e depois verificamos se existe pelo menos um registroi no banco de dados 
                 return $resultado;
             }
+
+            return [];
+
+            //fetchAll -> retorna um array com todos os resultados
         }
 
         public function update(Produto $p)
         {
+            $sql = 'UPDATE produtos SET n0ome = ?, descrição = ? WHERE  id = ?';
 
+            $stmt = Conexão::getConn()->prepare($sql);
+            //Associar os bind values
+            $stmt->bindValue(1, $p->getnnNNome());
+            $stmt->bindValue(2, $p->getDescrição());
+            $stmt->bindValue(3, $p->getId());
+
+            $stmt->execute();
         }
 
         public function delete($id)
         {
+            $sql = 'DELETE FROM produtos WHERE id = ?';
+
+            $stmt = Conexão::getConn()->prepare($sql);
+            $stmt->bindValue(1, $id);
+            $stmt->execute();
 
         }
     }
--- PHP/PHP_POO_2/aula_17/index.php
+++ PHP/PHP_POO_2/aula_17/index.php
@@ -14,10 +14,30 @@ $produto->setDescrição('i5, 4GB');
 
 //var_dump($produto);
 
+//CREATE
 $produtoDao = new \App\Model\ProdutoDao(); 
 $produtoDao->create($produto);
 $produtoDao->read();
 
+//READ - retorno página vazia
 foreach($produtoDao->read() as $produto) {
     echo $produto['nome']."<br>".$produto['descrição']."<hr>";
 }
+
+//seria uma array vazio 
+
+// UPDATE
+$produto->setId(8);
+$produto->setNome("Cadeira");
+$produto->setDescrição("Gamer");
+$produtoDao->update($produto);
+
+$produto->setId(7);
+$produto->setNome('Janela');
+$produto->setDescrição('Vidro');
+$produtoDao->update($produto);
+
+//DELETE
+//Somente passar o id do registro a ser exlcuído
+$produtoDao->delete(4);
+$produtoDao->delete(8);
