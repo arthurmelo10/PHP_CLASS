93bf3581518fccc85a89154df8dd6cbd16d8db66
arthur_melo <arthurmelo1000@gmail.com>
2022-05-26 09:10:03 -0300

    feat: conclusion of basic module

---
+++ udemy/curso-php/basico/comentarios.php
@@ -0,0 +1,26 @@
+<div class="titulo">Comentários PHP</div>
+
+<?php
+    echo "Estou no PHP";
+    // echo "Comentários";
+    #echo "Comentários";
+//?>
+
+<p>Depóis do primeiro bloco!</p>
+<!--
+    <p>Comentário HTML</p>
+    <p>Comentário HTML</p>
+    <p>Comentário HTML</p>
+-->
+
+<?php
+/*
+    Linha 1
+    Linha 2
+    Linha 3
+?> */
+
+?>
+
+<p>Depóis do segundo bloco!</p>
+
--- udemy/curso-php/index.php
+++ udemy/curso-php/index.php
@@ -16,7 +16,7 @@
         <div class="conteudo">
             <nav class="modulos">
                 <div class="modulo verde">
-                    <h3>Módulo 01</h3>
+                    <h3>Módulo 01 - Básico</h3>
                     <ul>
                         <li>
                             <a href="exercicio.php?dir=basico&file=ola">
@@ -26,6 +26,21 @@
                         <li>
                             <a href="exercicio.php?dir=basico&file=html">
                                 Integração HTML
+                            </a>
+                        </li>
+                        <li>
+                            <a href="exercicio.php?dir=basico&file=css">
+                                Integração CSS
+                            </a>
+                        </li>
+                        <li>
+                            <a href="exercicio.php?dir=basico&file=comentarios">
+                                Comentários PHP
+                            </a>
+                        </li>
+                        <li>
+                            <a href="exercicio.php?dir=basico&file=desafio">
+                                Desafio
                             </a>
                         </li>
                     </ul>
