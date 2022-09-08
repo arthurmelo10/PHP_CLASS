7fd0fe71c1ac19ada9e7800c73d1b93781551fd3
arthur_melo <arthurmelo1000@gmail.com>
2022-09-08 14:53:30 -0300

    feat: add theory about abstract class - part 1

--- udemy/curso-php/orientacao_objeto/abstract_teoria.php
+++ udemy/curso-php/orientacao_objeto/abstract_teoria.php
@@ -1,1 +1,31 @@
 <div class="titulo">Classe Abstrata - Teoria</div>
+
+<div>
+    <section>
+        <strong>Linha de % com a implementação de métodos</strong><br>
+        <br>Interface _______________Classe Abstrata_____________________ Classe Concreta 
+        <ul>
+            <li>Interface = (0% dos métodos implementados)</li>
+                <ul>
+                    <li>Define a assinatira do método</li>
+                </ul>
+            <li>Classe Concreta = (100% dos métodos implementados)</li>
+                <ul>
+                    <li>Pode ser instanciável</li>
+                    <li>Obrigatoriamente, tem que ter 100% dos métodos implementados</li>
+                </ul>
+            <li>Classe Abstrata</li>
+                <ul>
+                    <li>Pode ter todos os métodos sem corpo - <strong>abstratos</strong></li>
+                    <li>Pode ter todos os métodos implementados</li>
+                    <li>Não pode ser <strong>INSTANCIADA</strong></li>
+                    <li>Não deve se usar o new</li>
+                    <li>Pode ser utilizada em classes, que não há necessidade de total implementação, visto o grau de generalização da classe em questão </li>
+                </ul>
+        </ul>
+
+        <div>
+            Interface tem as suas vantagens, pois uma classe pode implementar várias interfaces aos mesmo tempo 
+        </div>
+    </section>    
+</div>
