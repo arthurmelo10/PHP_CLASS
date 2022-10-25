6f4cdd2038f1e455b2fc6e19e27167a3e3f2e409
arthur_melo <arthurmelo1000@gmail.com>
2022-10-25 14:04:00 -0300

    feat: add logout button

--- udemy/curso-php/exercicio.php
+++ udemy/curso-php/exercicio.php
@@ -21,9 +21,11 @@ if (!$_SESSION['usuario']) {
         <h2>Visualização do Exercício</h2>
     </header>
     <nav class="navegacao">
+        <span class="usuario">Usuário: <?= $_SESSION['usuario']?></span>    
         <a href<?="/{$_GET['dir']}/{$_GET['file']}.php" ?> 
         class="verde">Sem Formatação</a>
-        <a href="index.php" class="vermelho">Voltar</a>
+        <a href="index.php">Voltar</a>
+        <a href="logout.php" class="vermelho"> Sair </a>
     </nav>
     <main class="principal">
         <div class="conteudo">
--- udemy/curso-php/index.php
+++ udemy/curso-php/index.php
@@ -20,7 +20,8 @@ if (!$_SESSION['usuario']) {
         <h2>Índice dos Exercícios</h2>
     </header>
     <nav class="navegacao">
-        
+        <span class="usuario">Usuário: <?= $_SESSION['usuario']?></span>
+        <a href="logout.php" class="vermelho"> Sair </a>
     </nav>
     <main class="principal">
         <div class="conteudo">
