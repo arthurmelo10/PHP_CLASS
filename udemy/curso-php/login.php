97f32039530e4fdd57f4661d3eb25c495b72c205
arthur_melo <arthurmelo1000@gmail.com>
2022-10-28 09:04:15 -0300

    feat: create cookie for authentication pages

--- udemy/curso-php/login.php
+++ udemy/curso-php/login.php
@@ -14,6 +14,8 @@ if ($email) {
         if ($emailValido && $senhaValida) {
             $_SESSION['erros'] = null;
             $_SESSION['usuario'] = $usuario["nome"];
+            $exp = time() + 60 * 60 * 24 * 30;
+            setcookie('usuario', $usuario['nome'], $exp);
             header('Location: index.php');
         }
     }
