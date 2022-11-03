d321e27b821d552966ed8d147e1a308f2215bdf7
arthur_melo <arthurmelo1000@gmail.com>
2022-11-03 10:21:53 -0300

    feat: create the first repo of laracasts course

---
+++ laracasts/PHP/class_01/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Demo</title>
+</head>
+<body>
+    
+    <h1>
+        Hello World
+    </h1>
+
+    <h1>
+        <?php
+            echo 'Hello World PHP';
+        ?>
+    </h1>    
+</body>
+</html>
