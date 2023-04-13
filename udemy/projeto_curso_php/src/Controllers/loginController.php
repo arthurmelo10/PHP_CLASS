84fec13ca6f44837dd12b07dd2a428849c73f07d
arthur_melo <arthurmelo1000@gmail.com>
2023-04-13 20:21:55 -0300

    feat: create login validation

---
+++ udemy/projeto_curso_php/src/Controllers/loginController.php
@@ -0,0 +1,17 @@
+<?php
+
+loadModel('Login');
+$exception = null;
+
+if (count($_POST) > 0) {
+    $login = new Login($_POST);
+
+    try {
+        $user = $login->checkLogin();
+        echo "Usuário {$user->name} logado";
+    } catch (AppException $e) {
+        $exception = $e;
+    }
+}
+
+loadView('login', $_POST + ['exception' => $exception]);
--- udemy/projeto_curso_php/src/Models/Login.php
+++ udemy/projeto_curso_php/src/Models/Login.php
@@ -1,16 +1,43 @@
 <?php
-require_once(realpath(MODEL_PATH . '/User.php'));
+loadModel('User');
 
 class Login extends Model
 {
+    
+    public function validateLogin()
+    {
+        $errors = [];
+
+        if (!$this->email) {
+            $errors['email'] = 'E-mail é um campo obrigatório';
+        }
+
+        if (!$this->password) {
+            $errors['password'] = 'Por favor, informe a senha';    
+        }
+
+        if (count($errors) > 0) {
+            throw new ValidationException($errors);
+        }
+    }
+    
     public function checkLogin() 
     {
+        $this->validateLogin();
+
         $user = User::getOne(['email' => $this->email]);
+        
         if ($user) {
+            
+            if ($user->end_date) {
+                throw new AppException('Usuário desligado da empresa');
+            }
+            
             if (password_verify($this->password, $user->password)) {
                 return $user;
             }
         }
-        throw new Exception();
+
+        throw new AppException('Usuario/Senha inválidos');
     }
 }
