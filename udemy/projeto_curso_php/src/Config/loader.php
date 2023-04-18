7c9ebf613284bd7b580e48973744687f3c9fc97a
arthur_melo <arthurmelo1000@gmail.com>
2023-04-18 09:42:10 -0300

    feat: create components for pages template

--- udemy/projeto_curso_php/src/Config/loader.php
+++ udemy/projeto_curso_php/src/Config/loader.php
@@ -17,3 +17,19 @@ function loadView(string $viewName, array $params = []): void
     
     require_once(VIEW_PATH . "/$viewName.php");
 }
+
+function loadTemplateView(string $viewName, array $params = []): void 
+{
+    if (count($params) > 0) {
+        foreach ($params as $key => $value) {
+            if (strlen($key) > 0) {
+                ${$key} = $value;
+            }
+        }
+    }
+    
+    require_once(TEMPLATE_PATH . "/header.php");
+    require_once(TEMPLATE_PATH . "/left.php");
+    require_once(VIEW_PATH . "/$viewName.php");
+    require_once(TEMPLATE_PATH . "/footer.php");
+}
---
+++ udemy/projeto_curso_php/src/views/template/left.php
@@ -0,0 +1,30 @@
+<aside class="sidebar">
+    <nav class="menu mt-3">
+        <ul class="nav-list">
+            <li class="nav-item">
+                <a href="dayRecordsController.php">
+                    <i class="icofont-ui-check mr-2"></i>
+                    Registrar Ponto
+                </a>
+            </li>
+            <li class="nav-item">
+                <a href=".php">
+                    <i class="icofont-ui-calendar mr-2"></i>
+                    Registrar Mensal
+                </a>
+            </li>
+            <li class="nav-item">
+                <a href=".php">
+                    <i class="icofont-chart-histogram mr-2"></i>
+                    Relatório Gerencial
+                </a>
+            </li>
+            <li class="nav-item">
+                <a href=".php">
+                    <i class="icofont-users mr-2"></i>
+                    Usuários
+                </a>
+            </li>
+        </ul>
+    </nav>
+</aside>
