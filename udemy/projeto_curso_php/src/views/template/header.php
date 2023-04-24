a4a1c5c8c872092f9a22c4dfc076756013ff0032
arthur_melo <arthurmelo1000@gmail.com>
2023-04-24 11:19:40 -0300

    feat: add more details in the header

--- udemy/projeto_curso_php/src/views/template/header.php
+++ udemy/projeto_curso_php/src/views/template/header.php
@@ -25,18 +25,18 @@
         <div class="spacer"></div>
         <div class="dropdown">
             <div class="dropdown-button">
-                <span>Usuário Mock</span>
-                <i class="icofont-simple-down ml-2"></i>
+                <span class="ml-2">Usuário Mock</span>
+                <i class="icofont-simple-down mx-2"></i>
             </div>
             <div class="dropdown-content">
-                <ul class="nav-list">
-                    <li class="nav-item">
-                        <a href="logout.php">
-                            <i class="icofont-logout mr-2"></i>
-                            Sair
-                        </a>
-                    </li>
-                </ul>
-            </div>
+                    <ul class="nav-list">
+                        <li class="nav-item">
+                            <a href="logout.php">
+                                <i class="icofont-logout mr-2"></i>
+                                Sair
+                            </a>
+                        </li>
+                    </ul>
+                </div>
         </div>
     </header>
