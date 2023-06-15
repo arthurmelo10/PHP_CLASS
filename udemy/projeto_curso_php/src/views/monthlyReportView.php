e9a8e9903c274378039c60ef0f6064331dc08d43
arthur_melo <arthurmelo1000@gmail.com>
2023-06-15 11:55:17 -0300

    refact: add space on tag table into  monthly report view

--- udemy/projeto_curso_php/src/views/monthlyReportView.php
+++ udemy/projeto_curso_php/src/views/monthlyReportView.php
@@ -49,18 +49,18 @@
                 <?php foreach($report as $registry): ?>
                         <tr>
                             <td> <?= formatDateWihtLocale($registry->work_date, 'D, d/F/Y')?> </td>
-                            <td> <?= $registry->time1?> </td>
-                            <td> <?= $registry->time2?> </td>
-                            <td> <?= $registry->time3?> </td>
-                            <td> <?= $registry->time4?> </td>
+                            <td> <?= $registry->time1 ?> </td>
+                            <td> <?= $registry->time2 ?> </td>
+                            <td> <?= $registry->time3 ?> </td>
+                            <td> <?= $registry->time4 ?> </td>
                             <td> <?= $registry->getBalance() ?> </td>
                         </tr>
                     <?php endforeach ?>
                 <tr class="bg-primary text-white">
-                    <td>Horas Trabalhadas</td>
-                    <td colspan="3"><?= $sumOfWorkedTime ?></td>
-                    <td>Saldo Mensal</td>
-                    <td><?= $balance ?></td>
+                    <td> Horas Trabalhadas </td>
+                    <td colspan="3"> <?= $sumOfWorkedTime ?> </td>
+                    <td> Saldo Mensal </td>
+                    <td> <?= $balance ?> </td>
                 </tr>
             </tbody>
         </table>
