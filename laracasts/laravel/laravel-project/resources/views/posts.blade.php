fa3ae90a45230302d3aedfc43d353e53130e1c16
arthur_melo <arthurmelo1000@gmail.com>
2023-09-21 10:41:44 -0300

    feat:  create a new view with specifc posts into my blog application

--- laracasts/laravel/laravel-project/resources/views/posts.blade.php
+++ laracasts/laravel/laravel-project/resources/views/posts.blade.php
@@ -8,21 +8,21 @@
 </head>
     <body>
         <article>
-            <h1><a href="/post">My First Post</a></h1>
+            <h1><a href="/posts/my-first-post">My First Post</a></h1>
             <p>
                 Texto do primeiro post !!!
             </p>
         </article>
         
         <article>
-        <h1><a href="/post">My Second Post</a></h1>
+        <h1><a href="/posts/my-second-post">My Second Post</a></h1>
             <p>
                 Texto do segundo post !!!
             </p>
         </article>
 
         <article>
-        <h1><a href="/post">My Third Post</a></h1>
+        <h1><a href="/posts/my-third-post">My Third Post</a></h1>
             <p>
                 Texto do terceiro post !!!
             </p>
