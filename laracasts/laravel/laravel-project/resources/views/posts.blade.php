3ab6c4c1f5d6d5e8c8d57001bf1abafc52ba3cbd
arthur_melo <arthurmelo1000@gmail.com>
2023-10-21 11:21:21 -0300

    refact: modify the way of call posts on web file

--- laracasts/laravel/laravel-project/resources/views/posts.blade.php
+++ laracasts/laravel/laravel-project/resources/views/posts.blade.php
@@ -7,25 +7,10 @@
     <link rel="stylesheet" href="/app.css">
 </head>
     <body>
-        <article>
-            <h1><a href="/posts/my-first-post">My First Post</a></h1>
-            <p>
-                Texto do primeiro post !!!
-            </p>
-        </article>
-        
-        <article>
-        <h1><a href="/posts/my-second-post">My Second Post</a></h1>
-            <p>
-                Texto do segundo post !!!
-            </p>
-        </article>
-
-        <article>
-        <h1><a href="/posts/my-third-post">My Third Post</a></h1>
-            <p>
-                Texto do terceiro post !!!
-            </p>
-        </article>
+        <?php foreach ($posts as $post): ?>
+            <article>
+                <?=$post;?>
+            </article>
+        <?php endforeach?>
     </body>
 </html>
--- laracasts/laravel/laravel-project/routes/web.php
+++ laracasts/laravel/laravel-project/routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\Post;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,19 +15,21 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('posts');
+    
+    $posts = Post::all();
+    
+    return view('posts',[
+        'posts' => $posts
+    ]);
 });
 
 Route::get('posts/{post}', function ($slug) {
-    $path = __DIR__ . "/../resources/posts/{$slug}.html";
 
-    if (!file_exists($path)) {
-        return redirect('/');
-    }
+    $post = Post::find($slug);
 
-    $post = cache()->remember("posts.{$slug}",1200, function () use ($path){
-        return file_get_contents($path);
-    });
-
-    return view('post', ['post' => $post]);
+    return view('post',
+        [
+            'post'=> $post
+        ]
+    );
 })->where('posts', '[A-z_\-]+');
