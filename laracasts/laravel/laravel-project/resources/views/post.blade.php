3bea26152b9c99d423547a36c4c475cc7ceb506f
arthur_melo <arthurmelo1000@gmail.com>
2023-10-27 17:47:56 -0300

    feat: create a layout file to build the views

--- laracasts/laravel/laravel-project/resources/views/post.blade.php
+++ laracasts/laravel/laravel-project/resources/views/post.blade.php
@@ -1,21 +1,15 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-    <body>
-        <article>
-            <h1>
-                {{$post->title}}
-            </h1>
-            <div>
-                {!! $post->body !!}
-            </div>
-        </article>
-        <div style="margin-top: 1rem;">
-            <a href="/">Go back</a>
+@extends('layout')
+
+@section('content')
+    <article>
+        <h1>
+            {{$post->title}}
+        </h1>
+        <div>
+            {!! $post->body !!}
         </div>
-    </body>
-</html>
+    </article>
+    <div style="margin-top: 1rem;">
+        <a href="/">Go back</a>
+    </div>
+@endsection
--- laracasts/laravel/laravel-project/resources/views/posts.blade.php
+++ laracasts/laravel/laravel-project/resources/views/posts.blade.php
@@ -1,24 +1,17 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>My Blog</title>
-    <link rel="stylesheet" href="/app.css">
-</head>
-    <body>
-        @foreach ($posts as $post)
-            <article class="{{ $loop->even ? 'foobar' : '' }}">
-                <h1>
-                    <a href="/posts/{{ $post->slug }}">
-                        {{$post->title}}
-                    </a>
-                </h1>
+@extends ('layout')
 
-                <div>
-                    {{$post->excerpt}}
-                </div>
-            </article>
-        @endforeach
-    </body>
-</html>
+@section('content')
+    @foreach ($posts as $post)
+        <article class="{{ $loop->even ? 'foobar' : '' }}">
+            <h1>
+                <a href="/posts/{{ $post->slug }}">
+                    {{$post->title}}
+                </a>
+            </h1>
+
+            <div>
+                {{$post->excerpt}}
+            </div>
+        </article>
+    @endforeach
+@endsection
