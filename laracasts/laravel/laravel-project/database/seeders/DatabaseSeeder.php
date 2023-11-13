efa4b86f54f596f2d5292f0c9551f74ac0e57f07
arthur_melo <arthurmelo1000@gmail.com>
2023-11-13 16:07:01 -0300

    feat: create factory for Post and Category models

--- laracasts/laravel/laravel-project/database/seeders/DatabaseSeeder.php
+++ laracasts/laravel/laravel-project/database/seeders/DatabaseSeeder.php
@@ -16,51 +16,11 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        User::truncate();
-        Post::truncate();
-        Category::truncate();
-        
-        $user = User::factory(1)->create()->first();
-
-        $personal = Category::create([
-            'name' => 'Personal',
-            'slug' => 'personal',
-        ]);
-        
-        $work = Category::create([
-            'name' => 'Work',
-            'slug' => 'work',
-        ]);
-        
-        $family = Category::create([
-            'name' => 'Family',
-            'slug' => 'family',
+        $user = User::factory()->create([
+            'name' => 'Arthur Melo',
         ]);
 
-        Post::create([
-            'slug' => 'my-personal-post',
-            'title' => 'My Personal Post',
-            'excerpt' => 'Text 1',
-            'body' => '<p>My first  personal text using seed to put data on database</p>',
-            'category_id' => $personal->id,
-            'user_id' => $user->id,
-        ]);
-
-        Post::create([
-            'slug' => 'my-work-post',
-            'title' => 'My Work Post',
-            'excerpt' => 'Text 2',
-            'body' => '<p>My first work text using seed to put data on database</p>',
-            'category_id' => $work->id,
-            'user_id' => $user->id,
-        ]);
-
-        Post::create([
-            'slug' => 'my-family-post',
-            'title' => 'My Family Post',
-            'excerpt' => 'Text 1',
-            'body' => '<p>My first family text using seed to put data on database</p>',
-            'category_id' => $family->id,
+        Post::factory(5)->create([
             'user_id' => $user->id,
         ]);
     }
