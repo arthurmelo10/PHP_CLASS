b6b5eb69a121ac0f92dfd3559615b4749a573f08
arthur_melo <arthurmelo1000@gmail.com>
2023-11-22 09:15:26 -0300

    refact: build home page with post grid component

--- laracasts/laravel/laravel-project/resources/views/posts.blade.php
+++ laracasts/laravel/laravel-project/resources/views/posts.blade.php
@@ -2,17 +2,11 @@
     @include('_post-header')
 
     <main class="max-w-6xl mx-auto mt-6 lg:mt-20 space-y-6">
-        <x-post-featured-card/>
-        <div class="lg:grid lg:grid-cols-2">
-            <x-post-card/>
-            <x-post-card/>
-        </div>
-
-        <div class="lg:grid lg:grid-cols-3">
-            <x-post-card/>
-            <x-post-card/>
-            <x-post-card/>
-        </div>
+        @if ($posts->count())
+            <x-posts-grid :posts="$posts"/>
+        @else
+            <p class="text-center">No posts yet. Please check abck later</p>
+        @endif
     </main>
 
     <!-- @foreach ($posts as $post)
