643fe8d95e6103169cf28e50047b1e4d366f5dbf
arthur_melo <arthurmelo1000@gmail.com>
2024-01-15 10:56:01 -0300

    feat: create a nem category component into the home page

---
+++ laracasts/laravel/laravel-project/resources/views/components/category-dropdown.blade.php
@@ -0,0 +1,14 @@
+<x-dropdown>
+    <x-slot name="trigger">
+        <button class="py-2 pl-3 pr-9 text-sm font-semibold w-32 text-left inline-flex" style="width:100%; flex:1;">
+            {{ isset($currentCategory) ? ucwords($currentCategory->name) : "Categories" }}
+            <x-icon name="down-arrow" class="absolute pointer-events-none" style="right: 12px;"/>
+        </button>
+    </x-slot>
+    <x-dropdown-item href="/" :active="request()->routeIs('home')"> All </x-dropdown-item>
+        @foreach ($categories as $category)
+            <x-dropdown-item href="/categories/{{ $category->slug }}"
+                :active="request()->is('categories/' . $category->slug)"
+            > {{ ucwords($category->name) }} </x-dropdown-item>
+        @endforeach
+</x-dropdown>
