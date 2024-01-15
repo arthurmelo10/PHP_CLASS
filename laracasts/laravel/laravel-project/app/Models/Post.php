8d2794cc0e576f2f99ae5cf99fdc21541fb6e197
arthur_melo <arthurmelo1000@gmail.com>
2024-01-15 10:52:26 -0300

    feat: improve category search in the Post model

--- laracasts/laravel/laravel-project/app/Models/Post.php
+++ laracasts/laravel/laravel-project/app/Models/Post.php
@@ -41,6 +41,10 @@ class Post extends Model
             $query
                 ->where('title', 'like', '%' . $search. '%')
                 ->orwhere('body', 'like', '%' . $search . '%'));
+                
+        $query->when($filters['category'] ?? false, fn ($query, $category) =>
+            $query->whereHas('category', fn($query) => 
+                $query->where('slug', $category)));    
     }
 
     public function category(): BelongsTo
