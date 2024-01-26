6b34645bb5a4b514aa469434921c62ae0fa25276
arthur_melo <arthurmelo1000@gmail.com>
2024-01-26 18:05:16 -0300

    fix: correct the command to hash the password of the user

--- laracasts/laravel/laravel-project/app/Models/User.php
+++ laracasts/laravel/laravel-project/app/Models/User.php
@@ -42,12 +42,11 @@ class User extends Authenticatable
      */
     protected $casts = [
         'email_verified_at' => 'datetime',
-        'password' => 'hashed',
     ];
 
     public function setPasswordAttribute($password): void
     {
-        $this->atributes['passowrd'] = bcrypt($password);
+        $this->attributes['password'] = bcrypt($password);
     }
 
     public function posts(): HasMany
