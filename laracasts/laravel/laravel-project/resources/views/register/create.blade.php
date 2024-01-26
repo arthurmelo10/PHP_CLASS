5551932e5f789bf38450994091d4b676ba8bc8fa
arthur_melo <arthurmelo1000@gmail.com>
2024-01-26 17:23:09 -0300

    feat: add failed message to register view

--- laracasts/laravel/laravel-project/resources/views/register/create.blade.php
+++ laracasts/laravel/laravel-project/resources/views/register/create.blade.php
@@ -18,8 +18,14 @@
                     type="text"
                     name="name"
                     id="name"
+                    value="{{ old('name') }}"
                     required
                     >
+
+                    @error('name')
+                        <p class="text-red-500 text-xs mt-1">{{ $message }}</p>
+                    @enderror
+
                 </div>
             
                 <div class="mb-6">
@@ -34,8 +40,13 @@
                     type="text"
                     name="username"
                     id="username"
+                    value="{{ old('username') }}"
                     required
                     >
+
+                    @error('username')
+                        <p class="text-red-500 text-xs mt-1">{{ $message }}</p>
+                    @enderror
                 </div>
 
                 <div class="mb-6">
@@ -50,8 +61,13 @@
                     type="email"
                     name="email"
                     id="email"
+                    value="{{ old('email') }}"
                     required
                     >
+
+                    @error('email')
+                        <p class="text-red-500 text-xs mt-1">{{ $message }}</p>
+                    @enderror
                 </div>
 
                 <div class="mb-6">
@@ -68,6 +84,10 @@
                     id="password"
                     required
                     >
+
+                    @error('password')
+                        <p class="text-red-500 text-xs mt-1">{{ $message }}</p>
+                    @enderror
                 </div>
 
                 <div class="mb-6">
@@ -76,6 +96,16 @@
                         Submit
                     </button>
                 </div>
+
+                
+                <!-- @if($errors->any())
+                    <ul>
+                        @foreach ($errors->all() as $error)
+                            <li class="text-red-500 text-xs">{{ $error }}</li>
+                        @endforeach
+                    </ul>
+                @endif -->
+
             </form>
         </main>
     </section>
