deb4bd0f11e27c4a79de0eb165430ada49f84c18
arthur_melo <arthurmelo1000@gmail.com>
2024-01-29 18:53:36 -0300

    refact: change login form

--- laracasts/laravel/laravel-project/resources/views/register/create.blade.php
+++ laracasts/laravel/laravel-project/resources/views/register/create.blade.php
@@ -96,16 +96,6 @@
                         Submit
                     </button>
                 </div>
-
-                
-                <!-- @if($errors->any())
-                    <ul>
-                        @foreach ($errors->all() as $error)
-                            <li class="text-red-500 text-xs">{{ $error }}</li>
-                        @endforeach
-                    </ul>
-                @endif -->
-
             </form>
         </main>
     </section>
