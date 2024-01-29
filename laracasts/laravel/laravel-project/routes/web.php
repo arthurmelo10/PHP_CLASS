cbf99dd316f1df26dd88a5513d74aa8274b9bbc7
arthur_melo <arthurmelo1000@gmail.com>
2024-01-29 18:55:11 -0300

    feat: create store method to redirect the user after the login request

--- laracasts/laravel/laravel-project/routes/web.php
+++ laracasts/laravel/laravel-project/routes/web.php
@@ -32,4 +32,5 @@ Route::get('register', [RegisterController::class, 'create'])->middleware('guest
 Route::post('register', [RegisterController::class, 'store'])->middleware(('guest'));
 
 Route::get('login', [SessionController::class, 'create'])->middleware('guest');
+Route::post('login', [SessionController::class, 'store'])->middleware('guest');
 Route::post('logout', [SessionController::class, 'destroy'])->middleware('auth');
