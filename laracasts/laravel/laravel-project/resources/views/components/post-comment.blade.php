1ab780bd5231c6dfd7308c405c355cc2e7864867
arthur_melo <arthurmelo1000@gmail.com>
2024-02-02 14:10:59 -0300

    fix: adjust the link of image on comment view

--- laracasts/laravel/laravel-project/resources/views/components/post-comment.blade.php
+++ laracasts/laravel/laravel-project/resources/views/components/post-comment.blade.php
@@ -2,7 +2,7 @@
 
 <article class="flex bg-gray-100 border border-gray-p-6 rounded-xl space-x-4">
     <div class="flex-shrink-0">
-        <img src="https://i.pravatar.cc/100 alt=" imagem_do_usuario" width="60" height="60" class="rounded-xl">
+        <img src="https://i.pravatar.cc/60?u={{ $comment->id }} alt=" imagem_do_usuario" width="60" height="60" class="rounded-xl">
     </div>
     <div>
         <header class="mb-4">
