2b56768e6abd0d07668374deb7354422f265f516
arthur_melo <arthurmelo1000@gmail.com>
2024-02-02 18:30:24 -0300

    feat: add commentary form to post page

--- laracasts/laravel/laravel-project/resources/views/posts/show.blade.php
+++ laracasts/laravel/laravel-project/resources/views/posts/show.blade.php
@@ -47,6 +47,28 @@
 
                     <div class="space-y-4 lg:text-lg leading-loose"> {!! $post->body !!} </div>
                 </div>
+
+                <section class="col-span-8 col-start-5 mt-10 space-y-6">
+                    <x-panel>
+                        <form action="#" method="post">
+                            @csrf
+
+                            <header class="flex items-center">
+                                <img src="https://i.pravatar.cc/60?u={{ auth()->id() }} alt="" width="40" height="40" class="rounded-full">
+                                <h2 class="ml-4">Want to participate?</h2>
+                            </header>
+
+                            <div class="mt-6">
+                                <textarea name="body" class="w-full text-sm focus:outline-none focus:ring" rows="5" placeholder="Quick, think somethig to say!"></textarea>
+                            </div>
+
+                            <div class="flex justify-end mt-6 pt-6 border-t border-gray-200">
+                                <button type="submit" class="bg-blue-500 text-white uppercase font-semibold text-xs py-2 px-10 rounded-2xl hover:bg-blue-600">Post</button>
+                            </div>
+
+                        </form>
+                    </x-panel>
+                </section>
                 <section class="col-span-8 col-start-5 mt-10 space-y-6">
                     @foreach ($post->comments as $comment)
                         <x-post-comment :comment="$comment" />
