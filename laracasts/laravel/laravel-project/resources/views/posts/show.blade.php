72f7dd20bf0caa2db6db9a0a9acd75568e8ae7f3
arthur_melo <arthurmelo1000@gmail.com>
2024-02-05 19:52:41 -0300

    feat: add condition to post form appear to the logged user

--- laracasts/laravel/laravel-project/resources/views/posts/show.blade.php
+++ laracasts/laravel/laravel-project/resources/views/posts/show.blade.php
@@ -49,25 +49,31 @@
                 </div>
 
                 <section class="col-span-8 col-start-5 mt-10 space-y-6">
-                    <x-panel>
-                        <form action="#" method="post">
-                            @csrf
+                    @auth
+                        <x-panel>
+                            <form action="/posts/{{ $post->slug }}/comments" method="post">
+                                @csrf
 
-                            <header class="flex items-center">
-                                <img src="https://i.pravatar.cc/60?u={{ auth()->id() }} alt="" width="40" height="40" class="rounded-full">
-                                <h2 class="ml-4">Want to participate?</h2>
-                            </header>
+                                <header class="flex items-center">
+                                    <img src="https://i.pravatar.cc/60?u={{ auth()->id() }} alt="" width="40" height="40" class="rounded-full">
+                                    <h2 class="ml-4">Want to participate?</h2>
+                                </header>
 
-                            <div class="mt-6">
-                                <textarea name="body" class="w-full text-sm focus:outline-none focus:ring" rows="5" placeholder="Quick, think somethig to say!"></textarea>
-                            </div>
+                                <div class="mt-6">
+                                    <textarea name="body" class="w-full text-sm focus:outline-none focus:ring" rows="5" placeholder="Quick, think somethig to say!"></textarea>
+                                </div>
 
-                            <div class="flex justify-end mt-6 pt-6 border-t border-gray-200">
-                                <button type="submit" class="bg-blue-500 text-white uppercase font-semibold text-xs py-2 px-10 rounded-2xl hover:bg-blue-600">Post</button>
-                            </div>
+                                <div class="flex justify-end mt-6 pt-6 border-t border-gray-200">
+                                    <button type="submit" class="bg-blue-500 text-white uppercase font-semibold text-xs py-2 px-10 rounded-2xl hover:bg-blue-600">Post</button>
+                                </div>
 
-                        </form>
-                    </x-panel>
+                            </form>
+                        </x-panel>
+                    @else
+                        <p class="font-semibold">
+                            <a href="/register" class="hover:underline"> Register </a> or <a href="/login" class="hover:underline"> Log in </a> to participate of our community.
+                        </p>
+                    @endauth
                 </section>
                 <section class="col-span-8 col-start-5 mt-10 space-y-6">
                     @foreach ($post->comments as $comment)
