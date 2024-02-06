5c5fcb6b944d9049221e724df3b51e71c8937d9d
arthur_melo <arthurmelo1000@gmail.com>
2024-02-06 09:24:07 -0300

    refact: exclude comments into comments migration

--- laracasts/laravel/laravel-project/database/migrations/2024_01_31_222911_create_comments_table.php
+++ laracasts/laravel/laravel-project/database/migrations/2024_01_31_222911_create_comments_table.php
@@ -13,10 +13,8 @@ return new class extends Migration
     {
         Schema::create('comments', function (Blueprint $table) {
             $table->id();
-            //$table->unsignedBigInteger('post_id');
             $table->foreignId('post_id')->constrained()->cascadeOnDelete();
             $table->foreignId('user_id')->constrained()->cascadeOnDelete();
-            //$table->unsignedBigInteger('user_id');
             $table->text('body');
             $table->timestamps();
 
