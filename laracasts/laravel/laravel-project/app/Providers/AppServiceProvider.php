5e3b2128b318b21015db3d93c3f6e4520e96a5d4
arthur_melo <arthurmelo1000@gmail.com>
2024-02-21 12:32:10 -0300

    feat: create newsletter interface

--- laracasts/laravel/laravel-project/app/Providers/AppServiceProvider.php
+++ laracasts/laravel/laravel-project/app/Providers/AppServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use App\Services\MailchimpNewsletter;
+use App\Services\Newsletter;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\ServiceProvider;
 
@@ -12,7 +14,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        //
+        $this->app->bind(Newsletter::class, MailchimpNewsletter::class);
     }
 
     /**
--- laracasts/laravel/laravel-project/app/Services/Newsletter.php
+++ laracasts/laravel/laravel-project/app/Services/Newsletter.php
@@ -2,30 +2,7 @@
 
 namespace App\Services;
 
-use App\Config\MailchimpConfig;
-use MailchimpMarketing\ApiClient;
-
-class Newsletter
+interface Newsletter
 {
-    public function __construct(private MailchimpConfig $config) {}
-    
-    public function subscribe(string $email, string $list = null)
-    {
-        $list ??= $this->config->getListId(); 
-        
-        return $this->client()->lists->addListMember($list, [
-            "email_address" => $email,
-            "status" => "subscribed",
-        ]);
-    }
-
-    private function client()
-    {
-        $mailchimp = new ApiClient();
-    
-        return $mailchimp->setConfig([
-            'apiKey' => $this->config->getApiKey(),
-            'server' => 'us8'
-        ]);
-    }
+    public function subscribe(string $email, string $list = null);
 }
