1fa365470845dd0315025969f4b59d6cf0a21c2d
arthur_melo <arthurmelo1000@gmail.com>
2024-03-04 11:32:35 -0300

    refact: on forms about create new posts and login

--- laracasts/laravel/laravel-project/resources/views/posts/create.blade.php
+++ laracasts/laravel/laravel-project/resources/views/posts/create.blade.php
@@ -1,40 +1,35 @@
 <x-layout>
-    <section class="py-8 max-w-md mx-auto">
-        <h1 class="text-lg font-bold mb-4">
-            Publish New Post
-        </h1>
-        <x-panel>
-            <form action="/admin/posts" method="post" enctype="multipart/form-data">
-                @csrf
+    <x-setting heading="Publish New Post">
+        <form action="/admin/posts" method="post" enctype="multipart/form-data">
+            @csrf
 
-                <x-form.input name="title"/>
-                <x-form.input name="slug"/>
-                <x-form.input_file name="thumbnail" type="file"/>
-                <x-form.textarea name="excerpt"/>
-                <x-form.textarea name="body"/>
-                
-                <x-form.field>
-                    <x-form.label name="category"/>
+            <x-form.input name="title"/>
+            <x-form.input name="slug"/>
+            <x-form.input_file name="thumbnail" type="file"/>
+            <x-form.textarea name="excerpt"/>
+            <x-form.textarea name="body"/>
 
-                    <select name="category_id" id="category">
-                        @php
-                            $categories = \App\Models\Category::all();
-                        @endphp
+            <x-form.field>
+                <x-form.label name="category"/>
 
-                        @foreach ($categories as $category)
-                            <option value="{{ $category->id }}" {{ old('category_id') == $category->id ? 'selected' : '' }} >
-                                {{ ucwords($category->name) }}
-                            </option>
-                        @endforeach
+                <select name="category_id" id="category">
+                    @php
+                        $categories = \App\Models\Category::all();
+                    @endphp
 
-                    </select>
+                    @foreach ($categories as $category)
+                        <option value="{{ $category->id }}" {{ old('category_id') == $category->id ? 'selected' : '' }} >
+                            {{ ucwords($category->name) }}
+                        </option>
+                    @endforeach
 
-                    <x-form.error name="category" />
-                </x-form.field>
+                </select>
 
-                <x-form.button>Publish</x-form.button>
-                
-            </form>
-        </x-panel>
-    </section>
+                <x-form.error name="category" />
+            </x-form.field>
+
+            <x-form.button>Publish</x-form.button>
+
+        </form>
+    </x-setting>
 </x-layout>
--- laracasts/laravel/laravel-project/resources/views/sessions/create.blade.php
+++ laracasts/laravel/laravel-project/resources/views/sessions/create.blade.php
@@ -1,59 +1,18 @@
 <x-layout>
     <section class="px-6 py-8">
-        <main class="max-w-lg mx-auto mt-10 bg-gray-100 border border-gray-200 p-6 rounded-xl">
-            <h1 class="text-center font-bold text-xl">Log In!</h1>
-            
-            <form method="POST" action="/login" class="mt-10">
-                @csrf
+        <main class="max-w-lg mx-auto mt-10">
+            <x-panel>
+                <h1 class="text-center font-bold text-xl">Log In!</h1>
 
-                <div class="mb-6">
-                    <label 
-                    class="block mb-2 uppercase font-bold text-xs text-gray-700"
-                    for="">
-                        Email
-                    </label>
+                <form method="POST" action="/login" class="mt-10">
+                    @csrf
 
-                    <input
-                    class="border border-gray-400 p-2 w-full" 
-                    type="email"
-                    name="email"
-                    id="email"
-                    value="{{ old('email') }}"
-                    required
-                    >
+                    <x-form.input name="email" type="email" autocomplete="username"/>
+                    <x-form.input name="password" type="password" autcomplete="new-password"/>
+                    <x-form.button>Log In</x-form.button>
 
-                    @error('email')
-                        <p class="text-red-500 text-xs mt-1">{{ $message }}</p>
-                    @enderror
-                </div>
-
-                <div class="mb-6">
-                    <label 
-                    class="block mb-2 uppercase font-bold text-xs text-gray-700"
-                    for="password">
-                        Password
-                    </label>
-
-                    <input
-                    class="border border-gray-400 p-2 w-full" 
-                    type="password"
-                    name="password"
-                    id="password"
-                    required
-                    >
-
-                    @error('password')
-                        <p class="text-red-500 text-xs mt-1">{{ $message }}</p>
-                    @enderror
-                </div>
-
-                <div class="mb-6">
-                    <button type="submit"
-                    class="bg-blue-400 text-white rounded py-2 px-4 houver:bg-blue-500">
-                        Log In
-                    </button>
-                </div>
-            </form>
+                </form>
+            </x-panel>
         </main>
     </section>
 </x-layout>
