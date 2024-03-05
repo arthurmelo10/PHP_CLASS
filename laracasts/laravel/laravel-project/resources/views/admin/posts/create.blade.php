860cd9d84a53bc768aacbc8fde1cc7451d464522
arthur_melo <arthurmelo1000@gmail.com>
2024-03-05 11:27:40 -0300

    feat: create a new directory to admin views

---
+++ laracasts/laravel/laravel-project/resources/views/admin/posts/create.blade.php
@@ -0,0 +1,35 @@
+<x-layout>
+    <x-setting heading="Publish New Post">
+        <form action="/admin/posts" method="post" enctype="multipart/form-data">
+            @csrf
+
+            <x-form.input name="title"/>
+            <x-form.input name="slug"/>
+            <x-form.input_file name="thumbnail" type="file"/>
+            <x-form.textarea name="excerpt"/>
+            <x-form.textarea name="body"/>
+
+            <x-form.field>
+                <x-form.label name="category"/>
+
+                <select name="category_id" id="category">
+                    @php
+                        $categories = \App\Models\Category::all();
+                    @endphp
+
+                    @foreach ($categories as $category)
+                        <option value="{{ $category->id }}" {{ old('category_id') == $category->id ? 'selected' : '' }} >
+                            {{ ucwords($category->name) }}
+                        </option>
+                    @endforeach
+
+                </select>
+
+                <x-form.error name="category" />
+            </x-form.field>
+
+            <x-form.button>Publish</x-form.button>
+
+        </form>
+    </x-setting>
+</x-layout>
