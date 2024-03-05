7c23786167199c985e37d42c90f77da4199db5d8
arthur_melo <arthurmelo1000@gmail.com>
2024-03-05 11:30:22 -0300

    refact: on layout and setting components

--- laracasts/laravel/laravel-project/resources/views/components/layout.blade.php
+++ laracasts/laravel/laravel-project/resources/views/components/layout.blade.php
@@ -38,7 +38,7 @@
                         <x-slot name="trigger">
                             <button class="text-xs font-bold uppercase">Welcome, {{ auth()->user()->name }}</button>
                         </x-slot>
-                        <x-dropdown-item href="/admin/dashboard">
+                        <x-dropdown-item href="/admin/posts" :active="request()->is('admin/posts')">
                             Dashboard
                         </x-dropdown-item>
                         <x-dropdown-item href="/admin/posts/create" :active="request()->is('admin/posts/create')">
