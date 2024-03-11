bdae37a5d0d496828503c6ee4510639bc438574d
arthur_melo <arthurmelo1000@gmail.com>
2024-03-11 16:02:02 -0300

    feat: delete admins controller middleware

--- laracasts/laravel/laravel-project/app/Http/Controllers/PostController.php
+++ laracasts/laravel/laravel-project/app/Http/Controllers/PostController.php
@@ -5,12 +5,19 @@ namespace App\Http\Controllers;
 use App\Models\Post;
 use Illuminate\Contracts\View\Factory;
 use Illuminate\Contracts\View\View;
+use Illuminate\Support\Facades\Gate;
 use Illuminate\Validation\Rule;
 
 class PostController extends Controller
 {
     public function index(): View|Factory
     {
+        /**
+         * can and cannot method always will return a boolean
+         * authorize method will return a 403 status code
+         * $this->authorize('admin');
+        */
+
         $posts = Post::latest()->filter(
             request(['search', 'category', 'author'])
             )->paginate(3)->withQueryString();
