8a80994a779e9473c14e8df7beb9b21ad4eaafb9
arthur_melo <arthurmelo1000@gmail.com>
2024-03-11 16:05:37 -0300

    feat: create a new authenticate tag into blade component

--- laracasts/laravel/laravel-project/resources/views/components/layout.blade.php
+++ laracasts/laravel/laravel-project/resources/views/components/layout.blade.php
@@ -38,12 +38,16 @@
                         <x-slot name="trigger">
                             <button class="text-xs font-bold uppercase">Welcome, {{ auth()->user()->name }}</button>
                         </x-slot>
-                        <x-dropdown-item href="/admin/posts" :active="request()->is('admin/posts')">
-                            Dashboard
-                        </x-dropdown-item>
-                        <x-dropdown-item href="/admin/posts/create" :active="request()->is('admin/posts/create')">
-                            New Post
-                        </x-dropdown-item>
+
+                        @admin
+                            <x-dropdown-item href="/admin/posts" :active="request()->is('admin/posts')">
+                                Dashboard
+                            </x-dropdown-item>
+                            <x-dropdown-item href="/admin/posts/create" :active="request()->is('admin/posts/create')">
+                                New Post
+                            </x-dropdown-item>
+                        @endadmin
+
                         <x-dropdown-item
                             href="#"
                             x-data="{}"
